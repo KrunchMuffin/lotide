fc7c7df8c960f8ea4c37983b32082572cf1c71b8
Derek Bowes <db@bowesmail.com>
2021-11-09 15:18:38 -0500

    added middle.js

---
+++ middle.js
@@ -0,0 +1,31 @@
+function eqArrays(arr1, arr2) { return !!arr1 && !!arr2 && !(arr1 < arr2 || arr2 < arr1); }
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log('Arrays are equal', arr1, arr2);
+  } else {
+    console.log('Arrays are NOT equal', arr1, arr2);
+  }
+};
+
+const middle = function(arr) {
+  let arrMiddle = [];
+  const arrCnt = arr.length;
+
+  if (arrCnt > 2) {
+    if (arrCnt % 2 === 0) {
+      // even
+      arrMiddle.push(arr[arrCnt / 2 - 1 | 0]);
+      arrMiddle.push(arr[arrCnt / 2 | 0]);
+    } else if (arrCnt % 2 === 1) {
+      // odd
+      arrMiddle.push(arr[arrCnt / 2 | 0]);
+    }
+  }
+  return arrMiddle;
+};
+middle([1, 2, 3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([]), []);
